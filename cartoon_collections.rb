a66c622dde9e07925ca33c93d9d99a53bab816a7
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-10 19:18:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,27 +1,22 @@
 require 'pry'
 
 def roll_call_dwarves(array)
-  array.each_with_index {|name, index|
+  array.each_with_index {|name, index| 
     puts "#{index + 1}. #{name}"}
 end
 
 def summon_captain_planet(array)
-  result = []
   array.collect {|word| word.capitalize + "!"}
 end
+
 def long_planeteer_calls(array)
-    array.any? do |word| 
-      word.size > 4
-    end
+  array.any? {|word| word.size > 4}
+  
 end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.find do|item| 
-  cheese_types.include?(item)
-   end 
+  array.find {|item| cheese_types.include?(item)}
   
-  
- 
 end
